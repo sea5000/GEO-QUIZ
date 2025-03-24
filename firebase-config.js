b03a2eb4459d4aaf53a8228f307d06fdd8159e2a
sea5000 <121135112+sea5000@users.noreply.github.com>
2025-03-24 15:12:43 +0100

    Major changes. Country highlihgts and updated style choice.
    Also added firebase config, as it is set up to deploy on firebase now.

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBL1pdJip7hCq-VZMKfizYMyspFLRgdHkk",
+	authDomain: "geo-quiz-7ad38.firebaseapp.com",
+	databaseURL: "https://geo-quiz-7ad38-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "geo-quiz-7ad38",
+	storageBucket: "geo-quiz-7ad38.firebasestorage.app",
+	messagingSenderId: "395928209564",
+	appId: "1:395928209564:web:f2a865c5652d1ad1a50559"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
